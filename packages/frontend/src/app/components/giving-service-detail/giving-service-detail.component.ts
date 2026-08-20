523bc996ca5b76e4fe80f4b994ab1c96113b4220
Sandra Ivanova <nlgsyi4cw3k0iq89q4212i9a0k85n1@bots.bitbucket.org>
2026-08-20 12:58:27 +0200

    DNEV-12: implemented delete and edit button

--- packages/frontend/src/app/components/giving-service-detail/giving-service-detail.component.ts
+++ packages/frontend/src/app/components/giving-service-detail/giving-service-detail.component.ts
@@ -1,32 +1,39 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ActivatedRoute } from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import { Observable } from 'rxjs';
 import { ApiService } from '../../services/api.service';
 import {
   CATEGORY_LABELS,
-  IGivingService, Location,
+  IGivingService, IUser, Location,
   LOCATION_LABELS,
   PRICE_UNIT_LABELS,
   PriceUnit,
   ServiceCategory
 } from "@dnevnica/shared";
 import {ReviewComponent} from "../review/review.component";
-import {FormsModule} from "@angular/forms";
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-giving-service-detail',
   standalone: true,
-  imports: [CommonModule, ReviewComponent, FormsModule],
+  imports: [CommonModule, ReviewComponent, FormsModule, ReactiveFormsModule],
   templateUrl: './giving-service-detail.component.html',
   styleUrls: ['./giving-service-detail.component.scss']
 })
 export class GivingServiceDetailComponent implements OnInit {
 
+  private readonly router = inject(Router);
   private readonly route = inject(ActivatedRoute);
   private readonly apiService = inject(ApiService);
+  private readonly fb = inject(FormBuilder);
 
   service$!: Observable<IGivingService>;
+  currentUser$!: Observable<IUser>;
+
+  isEditModalOpen = false;
+  isDeleteModalOpen = false;
+  currentServiceId!: number;
 
   priceUnitLabels = PRICE_UNIT_LABELS;
   categoryLabels=CATEGORY_LABELS;
@@ -36,11 +43,80 @@ export class GivingServiceDetailComponent implements OnInit {
   categories = Object.values(ServiceCategory);
   locations = Object.values(Location);
 
+  serviceForm: FormGroup = this.fb.group({
+    title: ['', Validators.required],
+    price: [0, [Validators.required, Validators.min(0)]],
+    priceUnit: [PriceUnit.HOUR, Validators.required],
+    category: [ServiceCategory.OTHER, Validators.required],
+    yearsOfExperience: [1, [Validators.required, Validators.min(0)]],
+    availability: [''],
+    phone: ['', Validators.required],
+    location: [Location.KOCANI, Validators.required],
+    description: ['', Validators.required],
+    imageUrl: ['']
+  });
+
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
     if (id) {
-      this.service$ = this.apiService.findOne(+id);
+      this.currentServiceId = +id;
+      this.service$ = this.apiService.findOne(this.currentServiceId);
     }
+    this.currentUser$ = this.apiService.getCurrentUser();
   }
+
+  openEditModal(service: IGivingService) {
+    this.serviceForm.patchValue({
+      title: service.title,
+      price: service.price,
+      priceUnit: service.priceUnit,
+      category: service.category,
+      yearsOfExperience: service.yearsOfExperience,
+      availability: service.availability,
+      phone: service.phone,
+      location: service.location,
+      description: service.description,
+      imageUrl: service.imageUrl
+    });
+    this.isEditModalOpen = true;
+  }
+
+  closeEditModal() {
+    this.isEditModalOpen = false;
+  }
+
+  updateService() {
+    if (this.serviceForm.invalid) {
+      this.serviceForm.markAllAsTouched();
+      return;
+    }
+
+    this.apiService.updateService(this.currentServiceId, this.serviceForm.value).subscribe({
+      next: () => {
+        this.service$ = this.apiService.findOne(this.currentServiceId);
+        this.closeEditModal();
+      },
+      error: (err) => console.error('Error while updating', err)
+    });
+  }
+
+  openDeleteModal() {
+    this.isDeleteModalOpen = true;
+  }
+
+  closeDeleteModal() {
+    this.isDeleteModalOpen = false;
+  }
+
+  confirmDelete() {
+    this.apiService.deleteService(this.currentServiceId).subscribe({
+      next: () => {
+        this.closeDeleteModal();
+        this.router.navigate(['/giving-services']);
+      },
+      error: (err) => console.error('Error while deleting', err)
+    });
+  }
+
 }
